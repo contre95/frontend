65a2375419909cf41a9de76050950b03cabafb5c
contre95 <lucascontre95@gmail.com>
2023-07-30 21:54:31 +0200

    Primeros pasos con nuxt

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-  
-</script>
 
-<template lang="">
-  <div>
-    Alert ! !
-  </div>
+<template>
+<div
+  class="mb-4 rounded-lg bg-neutral-50 px-6 py-5 text-base text-neutral-600"
+  role="alert">
+  A simple light alert with
+  <a href="#!" class="font-bold text-neutral-600">an example link</a>.
+  Give it a click if you like.
+</div>
 </template>
